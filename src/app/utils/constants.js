ef226e2451e329b9b3fc02ed5e4c89f2612653ca
Rajat2417 <rajatdeepsingh2417@gmail.com>
2025-01-23 16:05:34 +0530

    constant.js

--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -1,5 +1,5 @@
-// const url_prefix = "https://backend-todo-vert.vercel.app";
-const url_prefix = "http://localhost:5000";
+const url_prefix = "https://backend-todo-vert.vercel.app";
+// const url_prefix = "http://localhost:5000";
 
 
 export {url_prefix}
